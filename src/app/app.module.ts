899d55a07f2215370a4ce6fb131040492b956d1c
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-09-05 22:09:50 -0500

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import {AppComponent} from './app.component'
 
 @NgModule({
-    imports:[],
-    declarations:[],
-    bootstrap:[]
+    imports:[
+        BrowserModule
+    ],
+    declarations:[
+        AppComponent
+    ],
+    bootstrap:[
+        AppComponent
+    ]
 })
 export class AppModule {}
